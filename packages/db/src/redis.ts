3468d4dd597389de488f3bbe7f858c6d3b146f6c
Sebastian Krüll <51778048+sebastiankrll@users.noreply.github.com>
2025-11-27 21:04:02 +0100

    Fix deploy

--- packages/db/src/redis.ts
+++ packages/db/src/redis.ts
@@ -1,7 +1,10 @@
 import type { WsDelta } from "@sk/types/vatsim";
 import Redis from "ioredis";
 
-const redis = new Redis();
+const redis = new Redis({
+    host: process.env.REDIS_HOST || "localhost",
+    port: Number(process.env.REDIS_PORT) || 6379,
+});
 
 export async function rdsPubWsDelta(delta: WsDelta) {
 	redis.publish("ws:delta", JSON.stringify(delta));
