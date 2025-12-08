01600e11277908837e13571e84cd7cc312a40997
Sebastian Krüll <51778048+sebastiankrll@users.noreply.github.com>
2025-12-08 19:02:50 +0100

    Refactor track feature state management

--- apps/web/components/Map/utils/trackFeatures.ts
+++ apps/web/components/Map/utils/trackFeatures.ts
@@ -7,18 +7,27 @@ import Style from "ol/style/Style";
 import { fetchTrackPoints } from "@/storage/cache";
 import { trackSource } from "./dataLayers";
 
-let pilotId: string | null = null;
-let lastPoint: [number, number] | null = null;
-let currentIndex: number = 0;
+interface Cached {
+	id: string | null;
+	coords: [number, number];
+	index: number;
+	stroke?: Stroke;
+}
+
+const cached: Cached = {
+	id: null,
+	coords: [0, 0],
+	index: -1,
+};
 
 export async function initTrackFeatures(id: string | null): Promise<void> {
 	if (!id) return;
 	const trackPoints = await fetchTrackPoints(id.replace("pilot_", ""));
 	const trackFeatures: Feature<LineString>[] = [];
 
-	for (currentIndex = 0; currentIndex < trackPoints.length - 1; currentIndex++) {
-		const start = trackPoints[currentIndex];
-		const end = trackPoints[currentIndex + 1];
+	for (cached.index = 0; cached.index < trackPoints.length - 1; cached.index++) {
+		const start = trackPoints[cached.index];
+		const end = trackPoints[cached.index + 1];
 
 		const trackFeature = new Feature({
 			geometry: new LineString(
@@ -29,40 +38,46 @@ export async function initTrackFeatures(id: string | null): Promise<void> {
 			),
 			type: "track",
 		});
-		const style = new Style({ stroke: getTrackSegmentColor(end.altitude_agl, end.altitude_ms) });
+		const stroke = getTrackSegmentColor(end.altitude_agl, end.altitude_ms);
+		const style = new Style({ stroke: stroke });
 
 		trackFeature.setStyle(style);
-		trackFeature.setId(`track_${id}_${currentIndex}`);
+		trackFeature.setId(`track_${id}_${cached.index}`);
 		trackFeatures.push(trackFeature);
 
-		if (currentIndex === trackPoints.length - 2) {
-			lastPoint = [end.longitude, end.latitude];
+		if (cached.index === trackPoints.length - 2) {
+			cached.coords = [end.longitude, end.latitude];
+			cached.stroke = stroke;
 		}
 	}
 
 	trackSource.clear();
 	trackSource.addFeatures(trackFeatures);
-	pilotId = id;
+	cached.id = id;
 }
 
 export async function updateTrackFeatures(delta: PilotDelta): Promise<void> {
 	if (trackSource.getFeatures().length === 0) return;
 
-	const pilot = delta.updated.find((p) => `pilot_${p.id}` === pilotId);
-	if (!pilotId || !pilot) return;
+	const pilot = delta.updated.find((p) => `pilot_${p.id}` === cached.id);
+	if (!cached.id || !pilot) return;
+	if (pilot.latitude === undefined || pilot.longitude === undefined) return;
 
 	const trackFeature = new Feature({
-		geometry: new LineString([lastPoint ?? [pilot.longitude, pilot.latitude], [pilot.longitude, pilot.latitude]].map((coord) => fromLonLat(coord))),
+		geometry: new LineString([cached.coords, [pilot.longitude, pilot.latitude]].map((coord) => fromLonLat(coord))),
 		type: "track",
 	});
-	const style = new Style({ stroke: getTrackSegmentColor(pilot.altitude_agl, pilot.altitude_ms) });
+
+	const stroke =
+		pilot.altitude_agl !== undefined && pilot.altitude_ms !== undefined ? getTrackSegmentColor(pilot.altitude_agl, pilot.altitude_ms) : cached.stroke;
+	const style = new Style({ stroke: stroke });
 
 	trackFeature.setStyle(style);
-	trackFeature.setId(`track_${pilot.id}_${++currentIndex}`);
+	trackFeature.setId(`track_${pilot.id}_${++cached.index}`);
 
 	trackSource.addFeature(trackFeature);
 
-	lastPoint = [pilot.longitude, pilot.latitude];
+	cached.coords = [pilot.longitude, pilot.latitude];
 }
 
 function getTrackSegmentColor(altitude_agl: number, altitude_ms: number): Stroke {
